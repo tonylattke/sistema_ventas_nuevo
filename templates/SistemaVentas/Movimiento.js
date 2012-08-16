87effe67682c69a829e4a6f831b334cfb9760969
tonylattke <tonylattke@gmail.com>
2012-08-16 09:15:02 -0430

    Borrado de archivos

--- templates/SistemaVentas/Movimiento.js
+++
@@ -1,9 +0,0 @@
-//Class Movimiento:
-var Movimiento = Class.create({
-    
-    initialize: function(motivo, tipo, cantidad) {
-        this.motivo   = motivo;
-        this.tipo     = tipo;
-        this.cantidad = cantidad;
-    }
-});
