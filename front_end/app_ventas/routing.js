fbfb029db7707c965d92247e07fec8eff34c8663
Oliver Perez <oliver.a.perez.c@gmail.com>
2012-08-26 19:23:13 -0700

    Logout al hacer click en algun boton .logout

--- front_end/app_ventas/routing.js
+++ front_end/app_ventas/routing.js
@@ -12,18 +12,26 @@ $.Controller("Routing",
         init : function() {
         },
 
+        //Eventos de DOM:
+        ".logout click" : function() {
+            window.location.href = '_logout';
+        },
+
+        '#menu li click' : function(el, ev) {
+            $.route.attr('ventana', el.attr('id'));
+        },
+
+        //Eventos de URL Hash:
         "route" : function() {
+            //Si el hash esta vacio direcciona a ventas:
             $.route.attr('ventana', 'ventas');
         },
 
         "/:ventana route" : function(data) {
             PAGE['ventana_'+data.ventana]();
-        },
-        
-
-        '#menu li click' : function(el, ev) {
-            $.route.attr('ventana', el.attr('id'));
         }
+        
+        
     });
 
 });
