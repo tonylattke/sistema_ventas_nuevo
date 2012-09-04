48906bd9ee00654b75e516cae935eefd95f80887
Oliver Perez <oliver.a.perez.c@gmail.com>
2012-09-03 21:29:32 -0700

    Add: Agregado controlador PanelLateral
    Todavia le falta para estar listo

---
+++ front_end/app_ventas/controllers/panel_lateral/panel_lateral.js
@@ -0,0 +1,35 @@
+steal(
+    './css/style.css',
+    //'./views/init.ejs',
+    './views/titulo.ejs',
+    './views/boton.ejs',
+
+function($) {
+
+$.Controller("PanelLateral",
+/** @Static */ {
+    pluginName : "panel_lateral"
+
+}, /** @Prototype */ {
+    
+    init : function() {
+        //this.element.html($.View(CONTROLLERS + 'panel_lateral/views/init.ejs'));
+    },
+
+    agregar_titulo : function(nombre) {
+        var titulo = $( $.View(CONTROLLERS + 'panel_lateral/views/titulo.ejs', nombre) );
+        this.element.append(titulo);
+        return titulo;
+    },
+
+    agregar_boton : function(nombre, func) {
+        var boton = $( $.View(CONTROLLERS + 'panel_lateral/views/boton.ejs', nombre) );
+        this.element.append(boton);
+        if(func) boton.click(func);
+        return boton;
+    },
+
+    agregar_subpanel : function() {}
+});
+
+});
